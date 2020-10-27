c8b917a996db62b3619223d48014187c22828dfe
pape018 <61976319+pape018@users.noreply.github.com>
2020-10-27 14:51:00 +0100

    Yes

--- Prog1 Hangman provning/Prog1 Hangman provning/Program.cs
+++ Prog1 Hangman provning/Prog1 Hangman provning/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Prog1_Hangman_provning
 {
@@ -6,22 +7,67 @@ namespace Prog1_Hangman_provning
     {
         static void Main(string[] args)
         {
+            //Börja med att välkomna spelaren till spelet. 
+            //Hitta ord och skriva ut med string.
             Console.WriteLine("Welcome to the game of Hang man");
-            string[] ordlsita = new string[3] { "Car", "Reddit", "Donut"};
-            string ordet = "hej";
+            string[] ordlista = new string[3] { "Car", "Key", "Song" };
+
+            Random random = new Random();
+            int slumpTal = random.Next(ordlista.Length);
+            string ord = ordlista[slumpTal];
+            char[] snabelof = new char[ord.Length];
+            
+            for (int i = 0; i < snabelof.Length; i = i +1)
+            {
+                snabelof[i] = '@';
+            }
+
             int antalGissningar = 10;
-            Console.WriteLine("Take a guess");
 
-            string gissning = Console.ReadLine();
+            do
 
-            if (ordet.Contains(gissning))
             {
+                Console.WriteLine("Tage a guess!");
+                string gissning = Console.ReadLine();
 
-            }
-            else
-            {
+                if (ord.Contains(gissning))
+                {
+                    char[] ordChars = ord.ToCharArray();
 
-            }
+                    for (int i = 0; i < ordChars.Length; i++)
+                    {
+                        if (gissning.ToCharArray()[0] == ordChars[i])
+                        {
+                            snabelof[i] = gissning.ToCharArray()[0];
+                        }
+                    }
+
+                    Console.WriteLine(snabelof);
+
+                    string starWord = new string(snabelof);
+
+                    if (starWord.Contains("@"))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("You have won the game!");
+                        Console.WriteLine("Wanna play again? It is fun");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Wrong guess, try again!");
+                    antalGissningar--;
+                    Console.WriteLine("You have: " + antalGissningar + " left!");
+                   
+                }
+            } while (antalGissningar > 0);
+            Console.WriteLine("Wanna paly again?");
+            
+
         }
     }
 }
