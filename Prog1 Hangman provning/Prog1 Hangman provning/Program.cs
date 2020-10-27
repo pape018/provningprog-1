5b8170e0b15e0953b188ace0bdfc523c7e75d844
pape018 <61976319+pape018@users.noreply.github.com>
2020-10-27 10:19:08 +0100

    push

---
+++ Prog1 Hangman provning/Prog1 Hangman provning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prog1_Hangman_provning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
